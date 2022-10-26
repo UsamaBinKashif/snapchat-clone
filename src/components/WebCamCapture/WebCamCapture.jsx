7be9760a9498ee3f72423935f21d58c45b961947
Usama Ahmed <80617842+UsamaBinKashif@users.noreply.github.com>
2022-10-26 18:45:49 +0500

    Update WebCamCapture.jsx

--- src/components/WebCamCapture/WebCamCapture.jsx
+++ src/components/WebCamCapture/WebCamCapture.jsx
@@ -10,7 +10,7 @@ import PeopleIcon from "@mui/icons-material/People";
 import { motion } from "framer-motion";
 const videoConstraints = {
    width: 250,
-    height: 500,
+    height: 300,
   facingMode: "user",
 };
 const WebCamCapture = () => {
